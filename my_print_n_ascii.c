066218dfc72fc631a58be314d82b4ba41fed5c24
Nadal <105127094+ZemmourGaming@users.noreply.github.com>
2024-05-25 10:41:22 +0000

    Returns the n first ascii characters

--- my_print_n_ascii.c
+++ my_print_n_ascii.c
@@ -3,8 +3,14 @@ void print_char(char c);
 void my_print_n_ascii(int howMany) {
     int i = 0;
     howMany += 32;
-    for(i=32; i<= howMany; i++){
+    if (howMany > 94){ 
+
+    }
+    else{
+
+        for(i=32; i<= howMany; i++){
         
-        print_char(i);
+            print_char(i);
+        }
     }
 }
